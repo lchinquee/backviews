a32ca266d1f0c324ad16d27d8e8eaa1ef5fcb44e
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-07-04 22:59:53 -0400

    add routes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
-const { newNote } = require('../../lib/notes');
+const { newNote, findById } = require('../../lib/notes');
 const { notes } = require('../../data/db');
 
 router.get('/db', (req, res) => {
-    let results = db;
+    console.log(notes);
+    return res.json(notes);
 });
+
+router.get('./db/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post('./db', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = newNote(req.body, notes);
+    res.json(note);
+})
 
 module.exports = router;
